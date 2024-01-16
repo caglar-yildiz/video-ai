445304d7cf9c9b51e40d332dd71f9007a3eb5a1f
Behzat Bilgin Erdem <bbilgin.erdem@gmail.com>
2024-01-16 19:08:25 +0300

    deleted unused favicon line

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ export const metadata: Metadata = {
 	title: "Play Next.js - SaaS Starter Kit and Boilerplate for Next.js",
 	description:
 		"Free Next.js SaaS Boilerplate and Starter Kit designed and built for SaaS startups. It comes with all necessary integrations, pages, and components you need to launch a feature-rich SaaS websites.",
-	icons: { icon: "/favicon.ico" },
 };
 
 type RootLayoutProps = Readonly<{
